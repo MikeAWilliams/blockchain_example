65834080741cf635400cc777b116bf60f89be1e3
Mike Williams <mumakin@gmail.com>
2021-02-27 23:35:14 -0700

    block validation works

--- blockchain/validate.go
+++ blockchain/validate.go
@@ -1,7 +1,25 @@
 package blockchain
 
+func sliceEqual(one []byte, two []byte) bool {
+	if len(one) != len(two) {
+		return false
+	}
+	for index, bOne := range one {
+		if bOne != two[index] {
+			return false
+		}
+	}
+	return true
+}
+
 func IsValid(previous Block, block Block, requiredLeadingZeros int) bool {
 	if previous.Index+1 != block.Index {
+		return false
+	}
+	if !sliceEqual(previous.Hash, block.PreviousHash) {
+		return false
+	}
+	if !HashIsHardEnough(block.Hash, requiredLeadingZeros) {
 		return false
 	}
 	return true
--- blockchain/validate_test.go
+++ blockchain/validate_test.go
@@ -14,9 +14,23 @@ func Test_BlockIsValid_Happy(t *testing.T) {
 	require.True(t, blockchain.IsValid(block1, block2, 2))
 }
 
-func Test_BlockIsValid_Stupid(t *testing.T) {
-	block1 := blockchain.Block{}
-	block2 := blockchain.Block{}
+func Test_BlockIsValid_BadIndex(t *testing.T) {
+	block1 := blockchain.Block{Index: 4, Hash: []byte{'0', '0', 'n'}}
+	block2 := blockchain.Block{Index: 6, PreviousHash: block1.Hash, Hash: []byte{'0', '0', 'q'}}
 
 	require.False(t, blockchain.IsValid(block1, block2, 0))
+}
+
+func Test_BlockIsValid_BadDoesNotMatchPrevious(t *testing.T) {
+	block1 := blockchain.Block{Index: 4, Hash: []byte{'0', '0', 'n'}}
+	block2 := blockchain.Block{Index: 5, PreviousHash: []byte{'0', '0', 'q'}}
+
+	require.False(t, blockchain.IsValid(block1, block2, 0))
+}
+
+func Test_BlockIsValid_HashInvalid(t *testing.T) {
+	block1 := blockchain.Block{Index: 4, Hash: []byte{'0', '0', 'n'}}
+	block2 := blockchain.Block{Index: 5, PreviousHash: block1.Hash}
+
+	require.False(t, blockchain.IsValid(block1, block2, 3))
 }
