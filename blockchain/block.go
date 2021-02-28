e0226c26c9995d78482e5d153782a0b79f12b5ac
Mike Williams <mumakin@gmail.com>
2021-02-27 22:20:20 -0700

    basic block

---
+++ blockchain/block.go
@@ -0,0 +1,57 @@
+package blockchain
+
+import (
+	"encoding/binary"
+	"hash"
+	"time"
+)
+
+type Block struct {
+	PreviousHash   []byte
+	CreatedTime    time.Time
+	Index          int64
+	MiningVariable int64
+	Data           string
+	Hash           []byte
+}
+
+func int64ToBytes(input int64) []byte {
+	result := []byte{}
+	binary.LittleEndian.PutUint64(result, uint64(input))
+	return result
+}
+
+func (b Block) ComputeHash(hasher hash.Hash) ([]byte, error) {
+	_, err := hasher.Write(b.PreviousHash)
+	if nil != err {
+		return nil, err
+	}
+	_, err = hasher.Write([]byte(b.CreatedTime.String()))
+	if nil != err {
+		return nil, err
+	}
+	_, err = hasher.Write([]byte(b.Data))
+	if nil != err {
+		return nil, err
+	}
+	_, err = hasher.Write(int64ToBytes(b.Index))
+	if nil != err {
+		return nil, err
+	}
+	_, err = hasher.Write(int64ToBytes(b.MiningVariable))
+	if nil != err {
+		return nil, err
+	}
+	hash := hasher.Sum(nil)
+	return hash, nil
+}
+
+func NewBlock(previousHash []byte, previousIndex int64, miningVariable int64, data string, hasher hash.Hash) (Block, error) {
+	result := Block{PreviousHash: previousHash, CreatedTime: time.Now(), Index: previousIndex + 1, MiningVariable: miningVariable, Data: data}
+	hash, err := result.ComputeHash(hasher)
+	if nil != err {
+		return Block{}, err
+	}
+	result.Hash = hash
+	return result, nil
+}
