0e05492eebf5f513ccdf99120b239c39c45e2f6b
Mike Williams <mumakin@gmail.com>
2021-03-02 19:38:10 -0700

    new block

--- network/network.go
+++ network/network.go
@@ -1,12 +1,22 @@
 package network
 
+import (
+	"blockchain_example/blockchain"
+	"math/rand"
+)
+
 type NewDataCallback func(string)
+type NewBlockCallback func(blockchain.Block)
 
 type Network struct {
-	newDataSubscribers []NewDataCallback
+	newDataSubscribers  []NewDataCallback
+	newBlockSubscribers []NewBlockCallback
 }
 
 func (n *Network) PostNewData(data string) {
+	rand.Shuffle(len(n.newDataSubscribers), func(i, j int) {
+		n.newDataSubscribers[i], n.newDataSubscribers[j] = n.newDataSubscribers[j], n.newDataSubscribers[i]
+	})
 	for _, callback := range n.newDataSubscribers {
 		callback(data)
 	}
@@ -14,4 +24,17 @@ func (n *Network) PostNewData(data string) {
 
 func (n *Network) SubscribeForNewData(callback NewDataCallback) {
 	n.newDataSubscribers = append(n.newDataSubscribers, callback)
+}
+
+func (n *Network) PostNewBlock(data blockchain.Block) {
+	rand.Shuffle(len(n.newBlockSubscribers), func(i, j int) {
+		n.newBlockSubscribers[i], n.newBlockSubscribers[j] = n.newBlockSubscribers[j], n.newBlockSubscribers[i]
+	})
+	for _, callback := range n.newBlockSubscribers {
+		callback(data)
+	}
+}
+
+func (n *Network) SubscribeForNewBlock(callback NewBlockCallback) {
+	n.newBlockSubscribers = append(n.newBlockSubscribers, callback)
 }
--- network/network_test.go
+++ network/network_test.go
@@ -1,13 +1,14 @@
 package network_test
 
 import (
+	"blockchain_example/blockchain"
 	"blockchain_example/network"
 	"testing"
 
 	"github.com/stretchr/testify/require"
 )
 
-func Test_BlockIsValid_Happy(t *testing.T) {
+func Test_NewData(t *testing.T) {
 	testObject := &network.Network{}
 	f1Called := 0
 	var f1Arg string
@@ -28,4 +29,28 @@ func Test_BlockIsValid_Happy(t *testing.T) {
 	require.Equal(t, 1, f2Called)
 	require.Equal(t, "TACO", f1Arg)
 	require.Equal(t, "TACO", f2Arg)
+}
+
+func Test_NewBlock(t *testing.T) {
+	testObject := &network.Network{}
+	f1Called := 0
+	var f1Arg blockchain.Block
+	testObject.SubscribeForNewBlock(func(arg blockchain.Block) {
+		f1Called++
+		f1Arg = arg
+	})
+	f2Called := 0
+	var f2Arg blockchain.Block
+	testObject.SubscribeForNewBlock(func(arg blockchain.Block) {
+		f2Called++
+		f2Arg = arg
+	})
+
+	expectedBlock := blockchain.Block{Index: 12, Data: "Taco"}
+	testObject.PostNewBlock(expectedBlock)
+
+	require.Equal(t, 1, f1Called)
+	require.Equal(t, 1, f2Called)
+	require.Equal(t, expectedBlock, f1Arg)
+	require.Equal(t, expectedBlock, f2Arg)
 }
