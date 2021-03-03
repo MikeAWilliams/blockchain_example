08b557f1d2fcb7f5be2060a6df0e0dec62a4bc13
Mike Williams <mumakin@gmail.com>
2021-03-03 05:52:51 -0700

    go for borke

--- exampleNetwork/main.go
+++ exampleNetwork/main.go
@@ -41,7 +41,7 @@ func main() {
 		wg.Done()
 	})
 
-	node.NewNode("First", &network, RequiredLeadingZeros, hashFactory)
+	node.NewNode("Miner1", &network, RequiredLeadingZeros, hashFactory)
 
 	// let the first node get the first block.
 	network.PostNewData("a")
@@ -49,7 +49,10 @@ func main() {
 
 	printChainStatus(network.GetBlochains())
 
-	node.NewNode("Second", &network, RequiredLeadingZeros, hashFactory)
+	node.NewNode("Miner2", &network, RequiredLeadingZeros, hashFactory)
+	node.NewNode("Miner3", &network, RequiredLeadingZeros, hashFactory)
+	node.NewNode("Miner4", &network, RequiredLeadingZeros, hashFactory)
+	node.NewNode("Miner5", &network, RequiredLeadingZeros, hashFactory)
 
 	simulatedData := []string{"b", "c", "d", "e", "f", "g", "h", "i", "j"}
 	for _, data := range simulatedData {
